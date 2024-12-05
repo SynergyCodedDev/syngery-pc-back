fc5c10594748bd3890ace9358961f95dd8024442
Thiago <thiagodelgado.dev@gmail.com>
2024-12-05 20:42:02 -0300

    Add init resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, AuthModule, UsersModule, CategoriesModule, TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
