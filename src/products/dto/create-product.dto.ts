1ac61f7b7ca8e614b0ca848ae9bbc9c2f77d146a
Thiago <thiagodelgado.dev@gmail.com>
2024-12-10 16:58:15 -0300

    init crud products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import { IsString, IsNumber, IsOptional, IsArray, IsBoolean, IsDateString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  categoryId: string;
+
+  @IsString()
+  desc: string;
+
+  @IsOptional()
+  @IsString()
+  subDesc?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  imageUrl: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  properties?: string[];
+
+  @IsString()
+  brand: string;
+
+  @IsBoolean()
+  warranty: boolean;
+
+  @IsOptional()
+  @IsDateString()
+  releaseDate?: Date;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const newProduct = this.productRepository.create(createProductDto);
+    return this.productRepository.save(newProduct);
   }
 
-  findAll() {
-    return `This action returns all products`;
+  findAll(): Promise<Product[]> {
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Promise<Product | null> {
+    return  this.productRepository.findOneBy({ id });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id); 
+    Object.assign(product, updateProductDto);
+    return this.productRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string): Promise<void> {
+    await this.productRepository.delete(id);
   }
 }
